e9fc1c5be5fb4ad253f7d5eed9abde4c311287c8
Настя Демидова <nastiademidova20@gmail.com>
2020-12-25 18:11:11 +0500

    Initial commit

---
+++ app/src/main/java/com/nastydemigod/tomb/MainActivity.java
@@ -0,0 +1,24 @@
+package com.nastydemigod.tomb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFind(View view) {
+        Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+        startActivity(intent);
+
+    }
+
+
+}
